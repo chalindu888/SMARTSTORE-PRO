b9df11cc5659262c4d872bda347c5150654a68a8
Chalindu Kalhara <chalindu888@gmail.com>
2026-02-24 01:21:04 +0530

    Configured Tailwind CSS with dark mode support

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts"></script>
 
+
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="min-h-screen bg-white dark:bg-black flex items-center justify-center">
+    <h1 class="text-black dark:text-white text-4xl font-bold">
+      Dark Mode Test
+    </h1>
+  </div>
 </template>
 
 <style scoped></style>
